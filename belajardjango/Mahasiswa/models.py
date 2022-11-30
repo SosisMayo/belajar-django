255031c14b63b713e902da210b55e0d50bcee645
SosisMayo <ramadani.dwicahya@gmail.com>
2022-11-30 12:10:07 +0700

    feat : connect djongo, make migrations

--- belajardjango/Mahasiswa/models.py
+++ belajardjango/Mahasiswa/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Mahasiswa(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    kelas = models.CharField(max_length=30, default='A')
+    nrp = models.CharField(max_length=30, default='0')
